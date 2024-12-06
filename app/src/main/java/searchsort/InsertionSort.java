2d967f3c81a8dec63e7b7240354e646ef994d89d
cyc9887 <cyc9887@gmail.com>
2024-12-06 14:41:00 -0500

    fixed indent

--- app/src/main/java/searchsort/InsertionSort.java
+++ app/src/main/java/searchsort/InsertionSort.java
@@ -5,7 +5,7 @@ public class InsertionSort {
      * Sort the array arr using insertion sort algorithm.
      */
     public static void sort(int[] arr) {
-for(int i = 1; i < arr.length; i++)
+        for(int i = 1; i < arr.length; i++)
         {
             for(int j = i; j > 0; j--)
             {
@@ -22,4 +22,5 @@ for(int i = 1; i < arr.length; i++)
             }
         }
     }
+}
 }
