33d30a9828a3240ad7c22e4684ea0620b2a1b8cc
cyc9887 <cyc9887@gmail.com>
2024-12-06 11:46:34 -0500

    Update Util.java

--- app/src/main/java/searchsort/Util.java
+++ app/src/main/java/searchsort/Util.java
@@ -33,7 +33,9 @@ public class Util {
      * Exchange the elements at indices i and j in the array arr.
      */
     public static void exch(int[] arr, int i, int j) {
-
+        int temp = i;
+        i = j;
+        j = temp;
     }
 
     /**
