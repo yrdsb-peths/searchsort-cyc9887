c0a54fc4cb9ca83d36255e743d2d75b311a7ff79
cyc9887 <cyc9887@gmail.com>
2024-12-06 14:18:21 -0500

    Update SelectionSort.java

--- app/src/main/java/searchsort/SelectionSort.java
+++ app/src/main/java/searchsort/SelectionSort.java
@@ -5,17 +5,6 @@ public class SelectionSort {
      * Sort the array arr using selection sort algorithm.
      */
     public static void sort(int[] arr) {
-        for(int i = 1; i < arr.length - 1; i++)
-        {
-            for(int j = i; j > 0; j--)
-            {
-                if(arr[j] > arr[j - 1])
-                {
-                    int temp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = temp;
-                }
-            }
-        }
+        
     }
 }
