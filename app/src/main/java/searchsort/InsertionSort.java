b36face711120a661231e96601ba606d9ba916d2
cyc9887 <cyc9887@gmail.com>
2024-12-06 14:19:02 -0500

    Update InsertionSort.java

--- app/src/main/java/searchsort/InsertionSort.java
+++ app/src/main/java/searchsort/InsertionSort.java
@@ -5,6 +5,21 @@ public class InsertionSort {
      * Sort the array arr using insertion sort algorithm.
      */
     public static void sort(int[] arr) {
-
+for(int i = 1; i < arr.length; i++)
+        {
+            for(int j = i; j > 0; j--)
+            {
+                if(arr[j] < arr[j - 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
     }
 }
