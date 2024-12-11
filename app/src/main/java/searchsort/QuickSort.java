8c4a1c0b40bf0865bc6e8c14f25f0cbbd9c4e129
cyc9887 <cyc9887@gmail.com>
2024-12-10 20:31:46 -0500

    quicksort

--- app/src/main/java/searchsort/QuickSort.java
+++ app/src/main/java/searchsort/QuickSort.java
@@ -6,23 +6,39 @@ public class QuickSort extends Sort {
      */
     @Override
     public void sort(int[] arr) {
-
+        Util.shuffle(arr, null);
+        quickSort(arr, 0, arr.length - 1);
     }
 
     /*
      * Sort the subarray arr[low..high] using quick sort algorithm.
      */
     private void quickSort(int[] arr, int low, int high) {
-
+        if (high <= low) return;
+        int p = partition(arr, low, high); 
+        quickSort(arr, low, p - 1); 
+        quickSort(arr, p + 1, high); 
     }
 
     /*
      * Partition the subarray arr[low..high] using the last element as pivot.
      * Return the index of the pivot element after partition.
      */
+    // hoare
     private int partition(int[] arr, int low, int high) {
-        return 0;
-
-    }
+        int i = low; 
+        int j = high + 1;
+        while (true) {
+            while (arr[++i] < arr[low])  // Find item on left to swap  
+                if (i == high) break; 
+            while (arr[--j] > arr[low]) 
+                if (j == low) break;
+            
+            if (i >= j) break;  // Check if pointers cross 
+            Util.exch(arr, i, j);  // Swap
+        } 
+        Util.exch(arr, low, j);  // Swap partitioning element  
+        return j;  // Return index of item now know to be in place
+    }    
 
 }
